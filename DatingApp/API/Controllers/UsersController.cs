4eb611257fdef65e6dfeeb68c6b90eb8d68db5f1
tony <songjf@yahoo.com>
2025-06-27 23:22:33 -0400

    section commit

---
+++ DatingApp/API/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace API.Controllers;
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+
+        Log.Information("ha ha ha ha ha");
+        var users = await context.Users.ToListAsync();
+        return users;
+    }
+
+    
+    // [HttpGet]
+    // public  IEnumerable<AppUser> GetUsers()
+    // {
+
+    //     Log.Information("ha ha ha ha ha");
+    //     var users =  context.Users.ToList();
+    //     return users;
+    // }
+
+    [HttpGet("{id:int}")]  // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+}
+
