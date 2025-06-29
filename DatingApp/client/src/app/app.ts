e3da174f5327fa01521ae933314eed7ba9d3f900
tony <songjf@yahoo.com>
2025-06-29 02:29:05 -0400

    commit section 228

--- DatingApp/client/src/app/app.ts
+++ DatingApp/client/src/app/app.ts
@@ -1,35 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit, TrackByFunction } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+
+// Define the User interface
+interface User {
+  id: number;
+  userName: string;
+  // Add other properties as needed
+}
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
 
+
    http = inject(HttpClient);
+  cdr = inject(ChangeDetectorRef);
    title = 'DatingApp';
-   users: any;
+   users: User[] = [];
+
 
   ngOnInit(): void {
 
      console.log("App component initialized");
 
-      const title123 = 'DatingApp';
-      const title456 = 'DatingApp';
-    this.http.get('https://localhost:5001/api/users')
+
+    this.http.get<User[]>('https://localhost:5001/api/users')
     .subscribe({
       next: response =>{
         this.users = response;
-        console.log(response)
+         this.cdr.detectChanges();
       } ,
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
-    })
+    });
+
+
+
   }
 
 }
